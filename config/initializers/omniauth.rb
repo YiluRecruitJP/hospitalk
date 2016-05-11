1b8e6a239c4e05c316c9c5efe326f6f15acb37ec
趙一路 <zhaoyilu@chouichiro-no-MacBook-Pro.local>
2016-05-11 17:48:44 +0900

    added app id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['heroku config:set FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, '1062592240482034', 'e06911b9ce9d644ac70526f77b04ebdd'
 end
 # heroku config:set FACEBOOK_KEY=0123456789
 # heroku config:set FACEBOOK_SECRET=0123456789
